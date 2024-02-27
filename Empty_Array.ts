10449a0b8655b7619fd16389b459090eeae38766
Abdul wasay khan <156195187+abdulwasayk14@users.noreply.github.com>
2024-02-27 13:52:21 +0500

    Add files via upload

---
+++ Empty_Array.ts
@@ -0,0 +1,14 @@
+
+//think of something you could store in an array
+let items :string[] = ['Pakistan','Karachi','Urdu'];
+
+//2nd method
+//Write a program that creates a list containing these items
+
+let item2 :string[] = [ ] ;
+
+item2.push('Germany');
+item2.push('England');
+item2.push('India');
+
+console.log(item2);
